1274daeead4dfd56e57e308c4c2338307e3f1351
ObaniGemini <robin@pi-et-ro.net>
2017-09-07 19:06:13 +0200

    Random stuff

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,6 +10,8 @@
 ynh_add_nginx_config () {
 	finalnginxconf="/etc/nginx/conf.d/$domain.d/$app.conf"
 	ynh_backup_if_checksum_is_different "$finalnginxconf"
+	pwd
+	read p
 	sudo cp ../conf/nginx.conf "$finalnginxconf"
 
 	# To avoid a break by set -u, use a void substitution ${var:-}. If the variable is not set, it's simply set with an empty variable.
